359889cacf93c5c2aeb500d082db0c142a55c254
Will Ockelmann-Wagner <wow@carbonfive.com>
2019-08-30 18:06:46 -0700

    prettier

--- javascript/webpacker_react-npm-module/src/configure-hot-module-replacement.js
+++ javascript/webpacker_react-npm-module/src/configure-hot-module-replacement.js
@@ -1,15 +1,15 @@
-import merge from 'webpack-merge'
+import merge from "webpack-merge";
 
 function configureHotModuleReplacement(origConfig) {
-  const config = origConfig
-  config.module.rules = config.module.rules.map((rule) => {
-    if (rule.loader === 'babel-loader') {
-      return merge(rule, { options: { plugins: ['react-hot-loader/babel'] } })
+  const config = origConfig;
+  config.module.rules = config.module.rules.map(rule => {
+    if (rule.loader === "babel-loader") {
+      return merge(rule, { options: { plugins: ["react-hot-loader/babel"] } });
     }
-    return rule
-  })
+    return rule;
+  });
 
-  return config
+  return config;
 }
 
-module.exports = configureHotModuleReplacement
+module.exports = configureHotModuleReplacement;
--- javascript/webpacker_react-npm-module/src/ujs.js
+++ javascript/webpacker_react-npm-module/src/ujs.js
@@ -4,52 +4,52 @@
 
 const ujs = {
   handleEvent(eventName, callback, { once } = { once: false }) {
-    const $ = (typeof window.jQuery !== 'undefined') && window.jQuery
+    const $ = typeof window.jQuery !== "undefined" && window.jQuery;
 
     if ($) {
       if (once) {
-        $(document).one(eventName, callback)
+        $(document).one(eventName, callback);
       } else {
-        $(document).on(eventName, callback)
+        $(document).on(eventName, callback);
       }
     } else {
-      document.addEventListener(eventName, callback, { once })
+      document.addEventListener(eventName, callback, { once });
     }
   },
 
   setup(onMount, onUnmount) {
-    const $ = (typeof window.jQuery !== 'undefined') && window.jQuery
-    const { Turbolinks } = window
+    const $ = typeof window.jQuery !== "undefined" && window.jQuery;
+    const { Turbolinks } = window;
 
     // Detect which kind of events to set up:
-    if (typeof Turbolinks !== 'undefined' && Turbolinks.supported) {
-      if (typeof Turbolinks.EVENTS !== 'undefined') {
+    if (typeof Turbolinks !== "undefined" && Turbolinks.supported) {
+      if (typeof Turbolinks.EVENTS !== "undefined") {
         // Turbolinks.EVENTS is in classic version 2.4.0+
-        this.turbolinksClassic(onMount, onUnmount)
-      } else if (typeof Turbolinks.controller !== 'undefined') {
+        this.turbolinksClassic(onMount, onUnmount);
+      } else if (typeof Turbolinks.controller !== "undefined") {
         // Turbolinks.controller is in version 5+
-        this.turbolinks5(onMount, onUnmount)
+        this.turbolinks5(onMount, onUnmount);
       } else {
-        this.turbolinksClassicDeprecated(onMount, onUnmount)
+        this.turbolinksClassicDeprecated(onMount, onUnmount);
       }
-    } else if ($ && typeof $.pjax === 'function') {
-      this.pjax(onMount, onUnmount)
+    } else if ($ && typeof $.pjax === "function") {
+      this.pjax(onMount, onUnmount);
     } else {
-      this.native(onMount)
+      this.native(onMount);
     }
   },
 
   turbolinks5(onMount, onUnmount) {
-    this.handleEvent('turbolinks:load', onMount, { once: true })
-    this.handleEvent('turbolinks:render', onMount)
-    this.handleEvent('turbolinks:before-render', onUnmount)
+    this.handleEvent("turbolinks:load", onMount, { once: true });
+    this.handleEvent("turbolinks:render", onMount);
+    this.handleEvent("turbolinks:before-render", onUnmount);
   },
 
   turbolinksClassic(onMount, onUnmount) {
-    const { Turbolinks } = window
+    const { Turbolinks } = window;
 
-    this.handleEvent(Turbolinks.EVENTS.CHANGE, onMount)
-    this.handleEvent(Turbolinks.EVENTS.BEFORE_UNLOAD, onUnmount)
+    this.handleEvent(Turbolinks.EVENTS.CHANGE, onMount);
+    this.handleEvent(Turbolinks.EVENTS.BEFORE_UNLOAD, onUnmount);
   },
 
   turbolinksClassicDeprecated(onMount, onUnmount) {
@@ -57,29 +57,29 @@ const ujs = {
     // have named events and didn't have a before-unload event.
     // Also, it didn't work with the Turbolinks cache, see
     // https://github.com/reactjs/react-rails/issues/87
-    const { Turbolinks } = window
-    Turbolinks.pagesCached(0)
-    this.handleEvent('page:change', onMount)
-    this.handleEvent('page:receive', onUnmount)
+    const { Turbolinks } = window;
+    Turbolinks.pagesCached(0);
+    this.handleEvent("page:change", onMount);
+    this.handleEvent("page:receive", onUnmount);
   },
 
   pjax(onMount, onUnmount) {
-    this.handleEvent('ready', onMount)
-    this.handleEvent('pjax:end', onMount)
-    this.handleEvent('pjax:beforeReplace', onUnmount)
+    this.handleEvent("ready", onMount);
+    this.handleEvent("pjax:end", onMount);
+    this.handleEvent("pjax:beforeReplace", onUnmount);
   },
 
   native(onMount) {
-    const $ = (typeof window.jQuery !== 'undefined') && window.jQuery
+    const $ = typeof window.jQuery !== "undefined" && window.jQuery;
     if ($) {
-      $(() => onMount())
-    } else if ('addEventListener' in window) {
-      document.addEventListener('DOMContentLoaded', onMount)
+      $(() => onMount());
+    } else if ("addEventListener" in window) {
+      document.addEventListener("DOMContentLoaded", onMount);
     } else {
       // add support to IE8 without jQuery
-      window.attachEvent('onload', onMount)
+      window.attachEvent("onload", onMount);
     }
   }
-}
+};
 
-export default ujs
+export default ujs;
