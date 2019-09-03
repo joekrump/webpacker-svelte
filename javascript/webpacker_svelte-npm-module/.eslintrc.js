d12e16e95d69989d399c2a47d0414a761e8b6334
Will Ockelmann-Wagner <wow@carbonfive.com>
2019-09-03 11:36:17 -0700

    format npm-module

--- javascript/webpacker_svelte-npm-module/.eslintrc.js
+++ javascript/webpacker_svelte-npm-module/.eslintrc.js
@@ -1,15 +1,15 @@
 module.exports = {
-  extends: "airbnb",
+  extends: ['airbnb-base', 'prettier'],
   rules: {
-    "comma-dangle": ["error", "never"],
-    "import/no-unresolved": "off",
-    "import/no-extraneous-dependencies": "off",
-    "import/extensions": "off",
-    "no-console": "off",
-    semi: ["error", "never"]
+    'comma-dangle': ['error', 'never'],
+    'import/no-unresolved': 'off',
+    'import/no-extraneous-dependencies': 'off',
+    'import/extensions': 'off',
+    'no-console': 'off',
+    semi: ['error', 'never']
   },
   env: {
     browser: true,
     node: true
   }
-};
+}
--- javascript/webpacker_svelte-npm-module/src/index.js
+++ javascript/webpacker_svelte-npm-module/src/index.js
@@ -1,38 +1,40 @@
-import intersection from "lodash/intersection";
-import keys from "lodash/keys";
-import assign from "lodash/assign";
-import omit from "lodash/omit";
-import ujs from "./ujs";
+import intersection from 'lodash/intersection'
+import keys from 'lodash/keys'
+import assign from 'lodash/assign'
+import omit from 'lodash/omit'
+import ujs from './ujs'
 
-const CLASS_ATTRIBUTE_NAME = "data-svelte-component";
-const PROPS_ATTRIBUTE_NAME = "data-svelte-props";
+const CLASS_ATTRIBUTE_NAME = 'data-svelte-component'
+const PROPS_ATTRIBUTE_NAME = 'data-svelte-props'
 
 const WebpackerSvelte = {
   registeredComponents: {},
 
   render(node, Component) {
-    const propsJson = node.getAttribute(PROPS_ATTRIBUTE_NAME);
-    const props = propsJson && JSON.parse(propsJson);
+    const propsJson = node.getAttribute(PROPS_ATTRIBUTE_NAME)
+    const props = propsJson && JSON.parse(propsJson)
 
-    new Component({
+    const c = new Component({
       target: node,
       props
-    });
+    })
+
+    console.log(c)
   },
 
   registerComponents(components) {
     const collisions = intersection(
       keys(this.registeredComponents),
       keys(components)
-    );
+    )
     if (collisions.length > 0) {
       console.error(
         `webpacker-svelte: can not register components. Following components are already registered: ${collisions}`
-      );
+      )
     }
 
-    assign(this.registeredComponents, omit(components, collisions));
-    return true;
+    assign(this.registeredComponents, omit(components, collisions))
+    return true
   },
 
   unmountComponents() {
@@ -44,36 +46,36 @@ const WebpackerSvelte = {
   },
 
   mountComponents() {
-    const { registeredComponents } = this;
-    const toMount = document.querySelectorAll(`[${CLASS_ATTRIBUTE_NAME}]`);
+    const { registeredComponents } = this
+    const toMount = document.querySelectorAll(`[${CLASS_ATTRIBUTE_NAME}]`)
 
     for (let i = 0; i < toMount.length; i += 1) {
-      const node = toMount[i];
-      const className = node.getAttribute(CLASS_ATTRIBUTE_NAME);
-      const component = registeredComponents[className];
+      const node = toMount[i]
+      const className = node.getAttribute(CLASS_ATTRIBUTE_NAME)
+      const component = registeredComponents[className]
 
       if (component) {
-        if (node.innerHTML.length === 0) this.render(node, component);
+        if (node.innerHTML.length === 0) this.render(node, component)
       } else {
         console.error(
           `webpacker-svelte: can not render a component that has not been registered: ${className}`
-        );
+        )
       }
     }
   },
 
   setup(components = {}) {
-    if (typeof window.WebpackerSvelte === "undefined") {
-      window.WebpackerSvelte = this;
+    if (typeof window.WebpackerSvelte === 'undefined') {
+      window.WebpackerSvelte = this
       ujs.setup(
         this.mountComponents.bind(this),
         this.unmountComponents.bind(this)
-      );
+      )
     }
 
-    window.WebpackerSvelte.registerComponents(components);
-    window.WebpackerSvelte.mountComponents();
+    window.WebpackerSvelte.registerComponents(components)
+    window.WebpackerSvelte.mountComponents()
   }
-};
+}
 
-export default WebpackerSvelte;
+export default WebpackerSvelte
