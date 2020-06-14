c6e799fdf2ecd262b23373d8e63b8f6e8ffc273b
Joseph Krump <krump.joseph@gmail.com>
2020-06-13 22:07:08 -0700

    Ignore linting dist files

--- javascript/webpacker-svelte/.eslintrc.js
+++ javascript/webpacker-svelte/.eslintrc.js
@@ -9,6 +9,7 @@ module.exports = {
     node: true,
     jest: true
   },
+  "ignorePatterns": ["dist/*"],
   extends: ['eslint:recommended', 'prettier'],
   rules: {
     'comma-dangle': ['error', 'never'],
